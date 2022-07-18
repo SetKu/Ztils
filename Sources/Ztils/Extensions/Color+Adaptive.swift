28b46aee519453faedf0c32018a2bc27a8138c53
SetKu <78518056+SetKu@users.noreply.github.com>
2022-07-18 17:14:47 -0600

    Added adaptive color initializers.

---
+++ Sources/Ztils/Extensions/Color+Adaptive.swift
@@ -0,0 +1,28 @@
+//
+//  Color+Adaptive.swift
+//  Ztils
+//
+//  Created by Zachary Morden on 2022-07-18.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+import UIKit
+
+public extension UIColor {
+    convenience init(light: UIColor, dark: UIColor) {
+        self.init(dynamicProvider: { traits in
+            traits.userInterfaceStyle == .light
+            ? light
+            : dark
+        })
+    }
+}
+
+public extension Color {
+    @available(iOS 15.0, *)
+    init(light: Color, dark: Color) {
+        self.init(uiColor: UIColor(light: UIColor(light), dark: UIColor(dark)))
+    }
+}
+#endif
