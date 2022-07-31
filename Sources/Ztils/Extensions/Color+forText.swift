051e21a78c975ac2eb3539701b9acd11aa6f5461
SetKu <78518056+SetKu@users.noreply.github.com>
2022-07-31 12:53:20 -0600

    Color extension fix and compatibility improvement.

--- Sources/Ztils/Extensions/Color+forText.swift
+++ Sources/Ztils/Extensions/Color+forText.swift
@@ -8,14 +8,28 @@
 #if canImport(SwiftUI)
 import SwiftUI
 
+#if canImport(UIKit)
+import UIKit
+#endif
+
+#if canImport(AppKit)
+import AppKit
+#endif
+
 public extension Color {
     // Solution derived from Stack Overflow blog post.
     // https://stackoverflow.com/questions/1855884/determine-font-color-based-on-background-color
     @available(iOS 14.0, *)
     static func forText(withBackground color: Color, threshold: CGFloat = 0.65) -> Color {
-        guard let components = color.cgColor?.components else { return Color.primary }
+        #if canImport(UIKit)
+        guard let components = UIColor(color).cgColor.components else { return Color.primary }
+        #endif
         
-        let luminance = (
+        #if canImport(AppKit)
+        guard let components = NSColor(color).cgColor.components else { return Color.primary }
+        #endif
+        
+        let luminance = 1 - (
             0.299 * components[0] +
             0.587 * components[1] +
             0.114 * components[2]
