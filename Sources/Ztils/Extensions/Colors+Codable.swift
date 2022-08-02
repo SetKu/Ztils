1f88001939e3cfde3cc6a86c946d5b5122e453c6
SetKu <78518056+SetKu@users.noreply.github.com>
2022-08-02 10:44:22 -0600

    Access control change for color conformances.

--- Sources/Ztils/Extensions/Colors+Codable.swift
+++ Sources/Ztils/Extensions/Colors+Codable.swift
@@ -10,8 +10,8 @@ import Foundation
 #if canImport(UIKit)
 import UIKit
 
-extension UIColor {
-    public func components(for traitCollection: UITraitCollection) -> ZTColorComponents {
+public extension UIColor {
+    func components(for traitCollection: UITraitCollection) -> ZTColorComponents {
         let resolved = self.resolvedColor(with: traitCollection)
         var components = ZTColorComponents(r: 0, g: 0, b: 0, a: 0)
         
@@ -25,7 +25,7 @@ extension UIColor {
         return components
     }
 
-    public func components(using colorScheme: UIUserInterfaceStyle) -> ZTColorComponents {
+    func components(using colorScheme: UIUserInterfaceStyle) -> ZTColorComponents {
         let collection = UITraitCollection(userInterfaceStyle: colorScheme)
         return components(for: collection)
     }
@@ -35,8 +35,8 @@ fileprivate enum UIColorCodingKeys: CodingKey {
     case light, dark
 }
 
-extension Decodable where Self: UIColor {
-    public init(from decoder: Decoder) throws {
+public extension Decodable where Self: UIColor {
+    init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: UIColorCodingKeys.self)
         let lightComponents = try container.decode(ZTColorComponents.self, forKey: .light)
         let darkComponents = try container.decode(ZTColorComponents.self, forKey: .dark)
@@ -51,8 +51,8 @@ extension Decodable where Self: UIColor {
     }
 }
 
-extension Encodable where Self: UIColor {
-    public func encode(to encoder: Encoder) throws {
+public extension Encodable where Self: UIColor {
+    func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: UIColorCodingKeys.self)
         let lightComponents = components(using: .light)
         let darkComponents = components(using: .dark)
@@ -69,8 +69,8 @@ fileprivate enum NSColorCodingKeys: CodingKey {
     case data
 }
 
-extension Decodable where Self: NSColor {
-    public init?(from decoder: Decoder) throws {
+public extension Decodable where Self: NSColor {
+    init?(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: NSColorCodingKeys.self)
         let data = try container.decode(Data.self, forKey: .data)
         let coder = try NSKeyedUnarchiver(forReadingFrom: data)
@@ -78,8 +78,8 @@ extension Decodable where Self: NSColor {
     }
 }
 
-extension Encodable where Self: NSColor {
-    public func encode(to encoder: Encoder) throws {
+public extension Encodable where Self: NSColor {
+    func encode(to encoder: Encoder) throws {
         let data = try NSKeyedArchiver.archivedData(withRootObject: self, requiringSecureCoding: true)
         var container = encoder.container(keyedBy: NSColorCodingKeys.self)
         try container.encode(data, forKey: .data)
